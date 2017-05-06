022b70add7162d15fb0127068ecdc9e8b5ae5bb0
Martin Zettwitz <martin.zettwitz@st.ovgu.de>
2017-05-06 14:28:06 +0200

    Updated data directory
    changed path into '<build>/data/101_ObjectCategories/'

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,19 @@
 
 int main(void)
 {
-std::cout << "Hello fellas!\n";
+    // Test for correct integration of opencv + contrib
+    std::cout << "Hello fellas!\n";
 
-cv::Vec2d vec = cv::Vec2d(0,0);
-std::cout << "CV Test: output of zero vector: x = " << vec[0] << ", y = " << vec[1] << std::endl;
+    cv::Vec2d vec = cv::Vec2d(0,0);
+    std::cout << "CV Test: output of zero vector: x = " << vec[0] << ", y = " << vec[1] << std::endl;
 
 
-// test if loading works
-cv::String path = "C:/101_ObjectCategories/*.jpg";
-Calltech_Image_Matrix img_Matrix;
-if (img_Matrix.loadImagesFromPath(path) != 0) return 0;
+    // test if loading works
+    // data organization: make a new directory 'data' in the build directory and extract the
+    // 'Caltech101'(101_ObjectCategories) data container into it
+    cv::String path = "data/101_ObjectCategories/*.jpg";
+    Calltech_Image_Matrix img_Matrix;
+    if (img_Matrix.loadImagesFromPath(path) != 0) return 0;
 
-return 0;
+    return 0;
 }
