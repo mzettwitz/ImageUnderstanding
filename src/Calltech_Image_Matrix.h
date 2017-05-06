93e5342f87e50d02532bd0af652e598ed6a1732e
daviddos <david.schulte@st.ovgu.de>
2017-05-06 06:55:39 +0200

    Added a class, basically vector< vector < cv::mat > >. Can load all jpgs from a given folder

---
+++ src/Calltech_Image_Matrix.h
@@ -0,0 +1,30 @@
+#ifndef CALLTECH_IMAGE_MATRIX_HPP
+#define CALLTECH_IMAGE_MATRIX_HPP
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+class Calltech_Image_Matrix
+{
+public:
+	Calltech_Image_Matrix();
+	~Calltech_Image_Matrix();
+
+	int loadImagesFromPath(cv::String);
+
+	std::vector < std::vector < cv::Mat > > getAllImages()	{ return m_all_image_data; };
+	std::vector < cv::Mat > getAllImagesOfIthClass(int i)	{ return m_all_image_data.at(i); };
+	cv::Mat getIthImageOfJthCategory(int i, int j)			{ return getAllImagesOfIthClass(j).at(i); }
+
+	std::vector < cv::String > getAllCategoriesNames()		{ return m_categories_names; };
+	cv::String getIthCategoryName(int i)					{ return m_categories_names.at(i); };
+
+private:
+	std::vector < std::vector < cv::Mat > > m_all_image_data;
+	std::vector < cv::String > m_categories_names;
+
+	size_t m_nr_categories;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <opencv2/core/core.hpp>
 #include <opencv2/tracking.hpp>
+#include "Calltech_Image_Matrix.h"
 
 int main(void)
 {
@@ -7,6 +9,12 @@ std::cout << "Hello fellas!\n";
 
 cv::Vec2d vec = cv::Vec2d(0,0);
 std::cout << "CV Test: output of zero vector: x = " << vec[0] << ", y = " << vec[1] << std::endl;
+
+
+// test if loading works
+cv::String path = "C:/101_ObjectCategories/*.jpg";
+Calltech_Image_Matrix img_Matrix;
+if (img_Matrix.loadImagesFromPath(path) != 0) return 0;
 
 return 0;
 }
