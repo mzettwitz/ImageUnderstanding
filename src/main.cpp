5258e8ecae5f77eb844199b029c2954db83df534
daviddos <david.schulte@st.ovgu.de>
2017-06-15 06:14:30 +0200

    training und predicten in der main

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 //#include <opencv2/tracking/feature.hpp>
 #include "include/Calltech_Image_Matrix.h"
 #include "include/KFoldValidation.h"
+#include <dlib/opencv.h>
 
 #include <dlib/gui_widgets.h>
 
@@ -28,66 +29,108 @@ int main(void)
 //	Validation.create10Fold(img_Matrix.getAllImages());
 	
 	std::vector < dlib::array2d<dlib::matrix<float, 31, 1> > > hog_training_features(20);
+	std::vector < dlib::array2d<dlib::matrix<float, 31, 1> > > hog_test_features(20);
 
 	for (int i = 0; i < 20; i++)
 	{
 		dlib::extract_fhog_features(img_Matrix.getIthImageOfJthCategory(i,0), hog_training_features[i]);
-		dlib::image_window win(img_Matrix.getIthImageOfJthCategory(i, 0));
-		dlib::image_window winhog(draw_fhog(hog_training_features[i]));
-		std::cout << std::endl << "Nr of row: " << hog_training_features[i].nr();
-		std::cout << std::endl << "Nr of columns: " << hog_training_features[i].nc();
-		system("Pause");
+		dlib::extract_fhog_features(img_Matrix.getIthImageOfJthCategory(i,i+1), hog_test_features[i]);
+	//	dlib::image_window win(img_Matrix.getIthImageOfJthCategory(i, 0));
+	//	dlib::image_window winhog(draw_fhog(hog_training_features[i]));
+
+		std::cout << std::endl << "Features generated";
 	}
 
-	/*
+	cv::Mat features,labels, testFeatures;
+	for (int i = 0; i < 20; i++)
+	{
+		cv::Mat flat_values;
+
+		
+		for (int j = 0; j < hog_training_features[0].nc(); j++)
+		{
+			for (int k = 0; k < hog_training_features[0].nr(); k++)
+			{
+				for (int l = 0; l < 31; l++)
+				{
+					flat_values.push_back(hog_training_features[i][j][k](l));
+				}
+			}
+		}
+		flat_values = flat_values.reshape(1, 1);
+		flat_values.convertTo(flat_values, CV_32F);
+		features.push_back(flat_values);	
+	}
+	//===========================TEST
+	for (int i = 0; i < 20; i++)
+	{
+		cv::Mat flat_values;
 
 
-	cv::Ptr< cv::CvFeatureParams > featureParams = cv::CvFeatureParams::create(0);
-	featureParams->init(*featureParams);
-	cv::Ptr < cv::CvHaarEvaluator > test_haar;
-	cv::Ptr < cv::CvHaarEvaluator::FeatureHaar > test_2;
-	test_2->eval(img1, cv::Rect(0, 0, 400, 400), feat);
-	test_haar->setImage(img1, '1', 1);
+		for (int j = 0; j < hog_test_features[0].nc(); j++)
+		{
+			for (int k = 0; k < hog_test_features[0].nr(); k++)
+			{
+				for (int l = 0; l < 31; l++)
+				{
+					flat_values.push_back(hog_test_features[i][j][k](l));
+				}
+			}
+		}
+		flat_values = flat_values.reshape(1, 1);
+		flat_values.convertTo(flat_values, CV_32F);
+		testFeatures.push_back(flat_values);
+	}
+	// ==============================
 	
-	auto test = test_haar->getNumFeatures();
-	cv::Mat features, labels;
-	features.push_back(features_1);
-	features.push_back(features_2);
-	features.push_back(features_3);
-	features.push_back(features_4);
-	features.push_back(features_5);
-	features.push_back(features_6);
-
+	labels.push_back(1);
 	labels.push_back(1);
 	labels.push_back(1);
 	labels.push_back(1);
 	labels.push_back(1);
 	labels.push_back(1);
 	labels.push_back(-1);
-
+	labels.push_back(1);
+	labels.push_back(1);
+	labels.push_back(-1);
+	labels.push_back(1);
+	labels.push_back(1);
+	labels.push_back(-1);
+	labels.push_back(1);
+	labels.push_back(1);
+	labels.push_back(1);
+	labels.push_back(1);
+	labels.push_back(1);
+	labels.push_back(1);
+	labels.push_back(-1);
 	
-	cv::CascadeClassifier test;
-	test.load("cascade.xml");
 	
 	cv::Ptr<cv::ml::Boost> model = model->create();
-	model = cv::Algorithm::load<cv::ml::Boost>("cascade.xml");
+	cv::Ptr<cv::ml::TrainData> train_data;
+	train_data = train_data->create(features, cv::ml::ROW_SAMPLE, labels);
+	std::cout << std::endl << "Begin Train";
+	model->train(train_data);
+	std::cout << std::endl << "End Train";
 
 	std::cout << model->isClassifier();
 	std::cout << model->isTrained();
 
-
-	std::cout << std::endl << model->predict(img_Matrix.getIthImageOfJthCategory(1, 1));
-	std::cout << std::endl << model->predict(img_Matrix.getIthImageOfJthCategory(1, 2));
-	std::cout << std::endl << model->predict(img_Matrix.getIthImageOfJthCategory(1, 3));
-	std::cout << std::endl << model->predict(img_Matrix.getIthImageOfJthCategory(1, 4));
-	std::cout << std::endl << model->predict(img_Matrix.getIthImageOfJthCategory(1, 5));
+	cv::Mat results;
+	model->predict(testFeatures,results);
+	std::cout << results;
+	/*
+	std::cout << std::endl << model->predict(dlib::toMat(img_Matrix.getIthImageOfJthCategory(6, 0)));
+	std::cout << std::endl << model->predict(dlib::toMat(img_Matrix.getIthImageOfJthCategory(20, 0)));
+	std::cout << std::endl << model->predict(dlib::toMat(img_Matrix.getIthImageOfJthCategory(2, 0)));
+	std::cout << std::endl << model->predict(dlib::toMat(img_Matrix.getIthImageOfJthCategory(5, 0)));
 	std::cout << std::endl;
-	std::cout << std::endl << model->predict(img_Matrix.getIthImageOfJthCategory(2, 2));
-	std::cout << std::endl << model->predict(img_Matrix.getIthImageOfJthCategory(3, 1));
-	std::cout << std::endl << model->predict(img_Matrix.getIthImageOfJthCategory(4, 2));
-	std::cout << std::endl << model->predict(img_Matrix.getIthImageOfJthCategory(5, 1));
-	std::cout << std::endl << model->predict(img_Matrix.getIthImageOfJthCategory(6, 2));
-
+	std::cout << std::endl << model->predict(dlib::toMat(img_Matrix.getIthImageOfJthCategory(1, 1)));
+	std::cout << std::endl << model->predict(dlib::toMat(img_Matrix.getIthImageOfJthCategory(2, 3)));
+	std::cout << std::endl << model->predict(dlib::toMat(img_Matrix.getIthImageOfJthCategory(5, 4)));
+	std::cout << std::endl << model->predict(dlib::toMat(img_Matrix.getIthImageOfJthCategory(1, 2)));
+	std::cout << std::endl << model->predict(dlib::toMat(img_Matrix.getIthImageOfJthCategory(4, 20)));
+	*/
+	
 	/*
 	KFoldValidation Validation;
 	Validation.create10Fold(img_Matrix.getAllImages());
