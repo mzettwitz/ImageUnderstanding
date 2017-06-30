3da3deecc8a51ae7ab80c9ddeb7208e563f86216
Martin Zettwitz <martin.zettwitz@st.ovgu.de>
2017-07-01 00:59:10 +0200

    export results to disk
    issue #27

--- src/main.cpp
+++ src/main.cpp
@@ -23,39 +23,59 @@ int main(void)
     // 'Caltech101'(101_ObjectCategories) data container into it
     cv::String path = "data/101_ObjectCategories/*.jpg";
     Calltech_Image_Matrix img_Matrix;
-    if (img_Matrix.loadImagesFromPath(path,64,64) != 0) return 0;
 
-    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    // SETUP
+    int imagesize = 64;
+    int cellsize = 8;
+    double nu = 0.00384434;//0.015;
+    string type = "SVM";
+    string setup = "imagesize ="  + std::to_string(imagesize) + " cellsize = " +
+            std::to_string(cellsize) + " type = " + type + " nu = " + std::to_string(nu);
+
+    if (img_Matrix.loadImagesFromPath(path,imagesize,imagesize) != 0) return 0;
+
+    while(nu < 0.019)
+    {
+        string setup = "imagesize ="  + std::to_string(imagesize) + " cellsize = " +
+                std::to_string(cellsize) + " type = " + type + " nu = " + std::to_string(nu);
+        std::cerr << nu << std::endl;
+        high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
+        KFoldValidation validation(img_Matrix.getNrCategories());
+        validation.create10Fold(img_Matrix.getAllImages(), cellsize, imagesize, nu);
+
+        //validation.printErrorMatrix();
+        //std::cout << "\n\nPlain RESULTS\n";
+
+        auto confMat = confMatrix(validation,img_Matrix);
+        printResults(confMat);
+
+        high_resolution_clock::time_point t2 = high_resolution_clock::now();
+        auto duration = duration_cast<seconds>( t2 - t1 ).count();
+        cerr << "time to compute: " << duration << " seconds" << std::endl;
+
+        storeMatrixOnDisk(confMat, duration, setup);
+
+        nu *= 1.1;
+    }
 
 
-	KFoldValidation validation(img_Matrix.getNrCategories());
-	validation.create10Fold(img_Matrix.getAllImages());
 
-	//validation.printErrorMatrix();
-	//std::cout << "\n\nPlain RESULTS\n";
 
-	auto confMat = confMatrix(validation,img_Matrix);
-	printResults(confMat);
 
-	high_resolution_clock::time_point t2 = high_resolution_clock::now();
-	auto duration = duration_cast<seconds>( t2 - t1 ).count();
-	cerr << "time to compute: " << duration << " seconds";
+    /*
+    std::vector < dlib::array2d<dlib::matrix<float, 31, 1> > > hog_training_features(30);
+    std::vector < dlib::array2d<dlib::matrix<float, 31, 1> > > hog_test_features(30);
 
-  
-
-/*
-	std::vector < dlib::array2d<dlib::matrix<float, 31, 1> > > hog_training_features(30);
-	std::vector < dlib::array2d<dlib::matrix<float, 31, 1> > > hog_test_features(30);
-	
-	for (int i = 0; i < 30; i++)
-	{
-		dlib::extract_fhog_features(img_Matrix.getIthImageOfJthCategory(i, 0), hog_training_features[i]);
-		dlib::extract_fhog_features(img_Matrix.getIthImageOfJthCategory(0, i + 1), hog_test_features[i]);
-		dlib::image_window win(img_Matrix.getIthImageOfJthCategory(i, 0));
-	    dlib::image_window winhog(draw_fhog(hog_training_features[i]));
-		system("Pause");
+    for (int i = 0; i < 30; i++)
+    {
+        dlib::extract_fhog_features(img_Matrix.getIthImageOfJthCategory(i, 0), hog_training_features[i]);
+        dlib::extract_fhog_features(img_Matrix.getIthImageOfJthCategory(0, i + 1), hog_test_features[i]);
+        dlib::image_window win(img_Matrix.getIthImageOfJthCategory(i, 0));
+        dlib::image_window winhog(draw_fhog(hog_training_features[i]));
+        system("Pause");
      std::cout << std::endl << "Features generated";
-			}
+            }
 */
     return 0;
 }
